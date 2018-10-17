f7c85de0ed14708c596107b4b805d73aecb2bfb1
PedroSchwarz <pedroschawarz97@gmail.com>
2018-10-16 22:50:51 -0300

    Register screen added

--- app/src/main/java/com/rodrigues/pedroschwarz/awesomeplaces/LoginActivity.java
+++ app/src/main/java/com/rodrigues/pedroschwarz/awesomeplaces/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.rodrigues.pedroschwarz.awesomeplaces;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.rodrigues.pedroschwarz.awesomeplaces.activity.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button logRegBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        logRegBtn = findViewById(R.id.log_reg_btn);
+
+        logRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/rodrigues/pedroschwarz/awesomeplaces/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.rodrigues.pedroschwarz.awesomeplaces.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rodrigues.pedroschwarz.awesomeplaces.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
